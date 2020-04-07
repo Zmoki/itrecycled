15f06b2dc305d7369bf13970b1a399ce0928e5fb
Zarema <zarema.khalilova@gmail.com>
2020-04-07 22:30:36 +0300

    Update types and categories, add examples

--- src/index.js
+++ src/index.js
@@ -1,12 +1,28 @@
 import { categories, types } from './types';
 
+const abbrToText = (abbr) => {
+  if (abbr === null || (abbr.iso == null && abbr.ru == null)) {
+    return '';
+  }
+
+  const list = [
+    ...(abbr.iso != null ? abbr.iso : []),
+    ...(abbr.ru != null ? abbr.ru : []),
+  ];
+
+  return (
+    list[0] + (list.length > 1 ? `</br>(${list.slice(1).join(', ')})` : '')
+  );
+};
 const $check = document.querySelector('.check');
 const $checkInput = document.querySelector('.check__input');
 const $checkError = document.querySelector('.check__error');
 const $result = document.querySelector('.result');
 const $resultNumber = document.querySelector('.result__number');
 const $resultAbbr = document.querySelector('.result__abbr');
+const $resultCategory = document.querySelector('.result__category');
 const $resultDescription = document.querySelector('.result__description');
+const $resultExamples = document.querySelector('.result__examples');
 const $resultRecycled = document.querySelector('.result__recycled');
 
 $check.addEventListener('submit', (e) => {
@@ -19,13 +35,22 @@ $check.addEventListener('submit', (e) => {
     if (!Number.isNaN(input) && parseInt(input, 0) === element.number) {
       return true;
     }
-    if (
-      Number.isNaN(element.number) &&
-      input.toUpperCase() === element.number
-    ) {
-      return true;
+    if (element.abbr !== null) {
+      if (
+        element.abbr.iso != null &&
+        element.abbr.iso.includes(input.toUpperCase())
+      ) {
+        return true;
+      }
+      if (
+        element.abbr.ru != null &&
+        element.abbr.ru.includes(input.toUpperCase())
+      ) {
+        return true;
+      }
     }
-    return element.abbr.includes(input.toUpperCase());
+
+    return false;
   });
 
   if (type !== undefined) {
@@ -33,11 +58,12 @@ $check.addEventListener('submit', (e) => {
 
     $result.style.setProperty('--color', category.color);
     $resultNumber.textContent = type.number;
-    $resultAbbr.innerText = type.abbr.join(' | ');
+    $resultAbbr.innerHTML = abbrToText(type.abbr);
+    $resultCategory.innerText = category.name;
     $resultDescription.innerText = type.description;
-    $resultRecycled.innerText = type.recycled
-      ? 'Перерабатывается'
-      : 'Не перерабатывается';
+    $resultExamples.innerText = `Примеры: ${type.examples}`;
+    $resultRecycled.innerText =
+      'Пока нет информации о переработке этого в России. Но скоро она появится.';
   } else {
     $checkError.innerText = 'Я не знаю такой код переработки';
   }
